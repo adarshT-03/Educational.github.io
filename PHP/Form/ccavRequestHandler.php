c8f15d647c5816e600f56d9e676bb244783c3ec1
adarshT-03 <79596188+adarshT-03@users.noreply.github.com>
2021-06-07 14:57:24 +0530

    Add files via upload

---
+++ PHP/Form/ccavRequestHandler.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+<title> Custom Form Kit </title>
+</head>
+<body>
+<center>
+
+<?php include('Crypto.php')?>
+<?php 
+
+	error_reporting(0);
+	
+	$merchant_data='328832';
+	$working_key='0C57B430D034C347276CF1D9233092CA';//Shared by CCAVENUES
+	$access_code='AVZN02IB10AM21NZMA';//Shared by CCAVENUES
+	
+	foreach ($_POST as $key => $value){
+		$merchant_data.=$key.'='.urlencode($value).'&';
+	}
+
+	$encrypted_data=encrypt($merchant_data,$working_key); // Method for encrypting the data.
+
+?>
+<form method="post" name="redirect" action="https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"> 
+<?php
+echo "<input type=hidden name=encRequest value=$encrypted_data>";
+echo "<input type=hidden name=access_code value=$access_code>";
+?>
+</form>
+</center>
+<script language='javascript'>document.redirect.submit();</script>
+</body>
+</html>
+
